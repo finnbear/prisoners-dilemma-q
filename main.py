32094ebf587008c7d5339a426c8940819a42c0d3
sanny720 <s-dandapanthulas@bsd405.org>
2017-11-30 18:12:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,75 @@
-# Configuration section
-population_size = 6 # How many AIs in the population
-mentor_instances = 1 # How many instances of each defined strategy there are
-episode_length = 10 # How many turns to play
-dve = 0.7 # During vs. ending reward
-training_time = 1 # How long to train in seconds per agent
-
-# Prisoner's dillema rewards [Player 1 reward, Player 2 reward]
-reward_matrix = [[[2, 2], # Both players cooperate
-                [0, 3], # Player 1 cooperates, player 2 defects
-                [3, 0], # Player 1 defects, player 2 cooperates
-                [1, 1]]] # Both players defect
-
 # Script section
 import sys
 import random
 from time import time
 from matplotlib import pyplot as plt
 
+# Configuration section
+population_size = 15  # How many AIs in the population
+mentor_instances = 2  # How many instances of each defined strategy there are
+episode_length = 100  # How many turns to play
+dve = 0.5  # During vs. ending reward
+training_time = 4  # How long to train in seconds per agent
+
+# Prisoner's dilemma rewards [Player 1 reward, Player 2 reward]
+reward_matrix = [[[2, 2],  # Both players cooperate
+                  [0, 3],  # Player 1 cooperates, player 2 defects
+                  [3, 0],  # Player 1 defects, player 2 cooperates
+                  [1, 1]]]  # Both players defect
+
+
 # Human agents pick which action to perform
-class Agent_Human:
-    def pick_action(self, state):
-        action = -1
+class AgentHuman:
+    @staticmethod
+    def pick_action(state):
+        action_p = -1
 
         # Print the given state
         print("State: " + str(state) + " (" + str(len(state)) + "/" + str(episode_length) + ")")
 
         # Repeat until valid input provided
-        while (action not in [0, 1]):
+        while action_p not in [0, 1]:
             try:
                 # Parse human's chosen action
-                action = int(raw_input("Choose Cooperate/Defect (0/1): "))
+                action_p = int(input("Choose Cooperate/Defect (0/1): "))
             except ValueError:
                 # Prompt human for valid input
                 print("Please input a number.")
-        
-        return action
-    def reward_action(self, state, action, reward):
+
+        return action_p
+
+    def reward_action(self, state, action_p, reward):
         pass
 
-# Q agents learn the best action to perform for every state encountered
-class Agent_Q:
-    def __init__(self, memory):
-        self.wins = 0 # Number of times agent has won an episode
-        self.losses = 0 # Number of times agent has lost an episode
-        self.Q = {} # Stores the quality of each action in relation to each state
-        self.memory = memory # The number of previous states the agent can factor into its decision
-        self.epsilon_counter = 1 # Inversely related to learning rate
 
-    def get_Q(self, state):
+# Q agents learn the best action to perform for every state encountered
+class AgentQ:
+    def __init__(self, memory):
+        self.wins = 0  # Number of times agent has won an episode
+        self.losses = 0  # Number of times agent has lost an episode
+        self.Q = {}  # Stores the quality of each action in relation to each state
+        self.memory = memory  # The number of previous states the agent can factor into its decision
+        self.epsilon_counter = 1  # Inversely related to learning rate
+
+    def get_q(self, state):
         quality1 = self.Q[str(state[-self.memory:])][0]
         quality2 = self.Q[str(state[-self.memory:])][1]
 
         return quality1, quality2
 
-    def set_Q(self, state, quality1, quality2):
+    def set_q(self, state, quality1, quality2):
         self.Q[str(state[-self.memory:])][0] = quality1
         self.Q[str(state[-self.memory:])][1] = quality2
 
-    def normalize_Q(self, state):
-        quality1, quality2 = self.get_Q(state)
+    def normalize_q(self, state):
+        quality1, quality2 = self.get_q(state)
 
         normalization = min(quality1, quality2)
 
-        self.set_Q(state, (quality1 - normalization) * 0.95, (quality2 - normalization) * 0.95)
+        self.set_q(state, (quality1 - normalization) * 0.95, (quality2 - normalization) * 0.95)
 
-    def max_Q(self, state):
-        quality1, quality2 = self.get_Q(state)
+    def max_q(self, state):
+        quality1, quality2 = self.get_q(state)
 
         if quality1 == quality2 or random.random() < (1 / self.epsilon_counter):
             return random.randint(0, 1)
@@ -78,19 +82,19 @@ class Agent_Q:
         # Decrease learning rate
         self.epsilon_counter += 0.5
 
-        # If the given state was never previously encounted
+        # If the given state was never previously encountered
         if str(state[-self.memory:]) not in self.Q:
             # Initialize it with zeros
             self.Q[str(state[-self.memory:])] = [0, 0]
-    
-        return self.max_Q(state)
 
-    def reward_action(self, state, action, reward):
+        return self.max_q(state)
+
+    def reward_action(self, state, action_r, reward):
         # Increase the quality of the given action at the given state
-        self.Q[str(state[-self.memory:])][action] += reward
+        self.Q[str(state[-self.memory:])][action_r] += reward
 
         # Normalize the Q matrix
-        self.normalize_Q(state)
+        self.normalize_q(state)
 
     def mark_victory(self):
         self.wins += 1
@@ -100,63 +104,64 @@ class Agent_Q:
 
     def analyse(self):
         # What percentage of games resulted in victory/defeat
-        percent_to_win = 0
+        percent_won_q = 0
         if self.wins > 0:
-            percent_to_win = float(self.wins) / (self.wins + self.losses)
-        
-        percent_to_lose = 0
+            percent_won_q = float(self.wins) / (self.wins + self.losses)
+        '''
+        percent_lost_q = 0
         if self.losses > 0:
-            percent_to_lose = float(self.losses) / (self.wins + self.losses)
-
+            percent_lost_q = float(self.losses) / (self.wins + self.losses)
+        '''
         # How many states will result in cooperation/defection
-        times_to_cooperate = 0
-        times_to_defect = 0
+        times_cooperated = 0
+        times_defected = 0
 
         for state in self.Q:
-            action = self.max_Q(eval(state))
+            action_s = self.max_q(eval(state))
 
-            if action == 0:
-                times_to_cooperate += 1
+            if action_s == 0:
+                times_cooperated += 1
             else:
-                times_to_defect += 1
+                times_defected += 1
 
         # What percentage of states will result in cooperation/defection
-        percent_to_cooperate = 0
-        if times_to_cooperate > 0:
-            percent_to_cooperate = float(times_to_cooperate) / len(self.Q)
-
-        percent_to_defect = 0
-        if times_to_defect > 0:
-            percent_to_defect = float(times_to_defect) / len(self.Q)
-
+        percent_cooperated = 0
+        if times_cooperated > 0:
+            percent_cooperated = float(times_cooperated) / len(self.Q)
+        '''
+        percent_defected = 0
+        if times_defected > 0:
+            percent_defected = float(times_defected) / len(self.Q)
+        '''
         # Return most relevant analysis
-        return self.wins, percent_to_win, percent_to_cooperate
+        return self.wins, percent_won_q, percent_cooperated
 
     def reset_analysis(self):
         self.wins = 0
         self.losses = 0
 
+
 # Defined agents know which action to perform
-class Agent_Defined:
+class AgentDefined:
     def __init__(self, strategy):
-        self.wins = 0 # Number of times agent has won an episode
-        self.losses = 0 # Number of times agent has lost an episode
+        self.wins = 0  # Number of times agent has won an episode
+        self.losses = 0  # Number of times agent has lost an episode
         self.strategy = strategy
 
     def pick_action(self, state):
-        if self.strategy == 0: # Tit for tat
-            if len(state) == 0: # On the first tern
-                return 0 # Cooperate
-            else: # Otherwise
-                return state[-1] # Pick the last action of the opponent
-        elif self.strategy == 1: # Holds a grudge
-            if 1 in state: # If the enemy has ever defected
-                return 1 # Defect
-            else: # Otherwise
-                return 0 # Cooperate
+        if self.strategy == 0:  # Tit for tat
+            if len(state) == 0:  # On the first tern
+                return 0  # Cooperate
+            else:  # Otherwise
+                return state[-1]  # Pick the last action of the opponent
+        elif self.strategy == 1:  # Holds a grudge
+            if 1 in state:  # If the enemy has ever defected
+                return 1  # Defect
+            else:  # Otherwise
+                return 0  # Cooperate
 
-    def reward_action(self, state, action, reward):
-        pass # Since these agents are defined, no learning occurs
+    def reward_action(self, state, action_r, reward):
+        pass  # Since these agents are defined, no learning occurs
 
     def mark_victory(self):
         self.wins += 1
@@ -166,16 +171,17 @@ class Agent_Defined:
 
     def analyse(self):
         # What percentage of games resulted in victory/defeat
-        percent_to_win = 0
+        percent_won = 0
         if self.wins > 0:
-            percent_to_win = float(self.wins) / (self.wins + self.losses)
-        
-        percent_to_lose = 0
+            percent_won = float(self.wins) / (self.wins + self.losses)
+        '''
+        percent_lost = 0
         if self.losses > 0:
-            percent_to_lose = float(self.losses) / (self.wins + self.losses)
-
+            percent_lost = float(self.losses) / (self.wins + self.losses)
+        '''
         # Return most relevant analysis
-        return self.wins, percent_to_win
+        return self.wins, percent_won
+
 
 # Stores all AIs
 population = []
@@ -190,12 +196,12 @@ mentors = []
 
 # Create a random AI with a random amount of memory
 for i in range(population_size):
-    population.append(Agent_Q(random.randint(2, 5)))
+    population.append(AgentQ(random.randint(2, 5)))
 
 # Create instances of defined strategies
-for i in range(2): # Number of defined strategies
+for i in range(2):  # Number of defined strategies
     for j in range(mentor_instances):
-        mentors.append(Agent_Defined(i))
+        mentors.append(AgentDefined(i))
 
 # Training time initialization
 start_time = time()
@@ -218,16 +224,16 @@ while remaining_time > 0:
 
         # Analyse population
         if time() > start_time + 0.5:
-            timestep = []
+            time_step = []
             for agent in population:
-                timestep.append(agent.analyse())
+                time_step.append(agent.analyse())
                 agent.reset_analysis()
-            population_analysis.append(timestep)
+            population_analysis.append(time_step)
 
-        # TODO: Analyse mentors
+            # TODO: Analyse mentors
 
-    state1 = [] # State visible to player 1 (actions of player 2)
-    state2 = [] # State visible to player 2 (actions of player 1)
+    state1 = []  # State visible to player 1 (actions of player 2)
+    state2 = []  # State visible to player 2 (actions of player 1)
 
     # Pick a random member of the population to serve as player 1
     player1 = random.choice(population)
@@ -238,11 +244,11 @@ while remaining_time > 0:
     for i in range(episode_length):
         action = None
 
-        action1 = player1.pick_action(state1) # Select action for player 1
-        action2 = player2.pick_action(state2) # Select action for player 2
+        action1 = player1.pick_action(state1)  # Select action for player 1
+        action2 = player2.pick_action(state2)  # Select action for player 2
 
-        state1.append(action2) # Log action of player 2 for player 1
-        state2.append(action1) # Log action of player 1 for player 2
+        state1.append(action2)  # Log action of player 2 for player 1
+        state2.append(action1)  # Log action of player 1 for player 2
 
     # Stores the total reward over all games in an episode
     total_reward1 = 0
@@ -252,28 +258,28 @@ while remaining_time > 0:
         action1 = state2[i]
         action2 = state1[i]
 
-        reward1 = 0 # Total reward due to the actions of player 1 in the entire episode
-        reward2 = 0 # Total reward due to the actions of player 2 in the entire episode
+        reward1 = 0  # Total reward due to the actions of player 1 in the entire episode
+        reward2 = 0  # Total reward due to the actions of player 2 in the entire episode
 
         # Calculate rewards for each player
-        if action1 == 0 and action2 == 0: # Both players cooperate
+        if action1 == 0 and action2 == 0:  # Both players cooperate
             reward1 = reward_matrix[0][0][0]
             reward2 = reward_matrix[0][0][1]
-        elif action1 == 0 and action2 == 1: # Only player 2 defects
+        elif action1 == 0 and action2 == 1:  # Only player 2 defects
             reward1 = reward_matrix[0][1][0]
             reward2 = reward_matrix[0][1][1]
-        elif action1 == 1 and action2 == 0: # Only player 1 defects
+        elif action1 == 1 and action2 == 0:  # Only player 1 defects
             reward1 = reward_matrix[0][2][0]
             reward2 = reward_matrix[0][2][1]
-        elif action1 == 1 and action2 == 1: # Both players defect
+        elif action1 == 1 and action2 == 1:  # Both players defect
             reward1 = reward_matrix[0][3][0]
             reward2 = reward_matrix[0][3][1]
 
         total_reward1 += reward1
         total_reward2 += reward2
 
-        player1.reward_action(state1[:i], action1, reward1 * dve) # Assign reward to action of player 1
-        player2.reward_action(state2[:i], action2, reward2 * dve) # Assign reward to action of player 2
+        player1.reward_action(state1[:i], action1, reward1 * dve)  # Assign reward to action of player 1
+        player2.reward_action(state2[:i], action2, reward2 * dve)  # Assign reward to action of player 2
 
     # Assign reward for winning player
     if total_reward1 > total_reward2:
@@ -312,18 +318,18 @@ for i in range(len(population_analysis[-1])):
     victories_percent_stack_colors.append(str(percent_to_cooperate))
 
 i = 0
-for timestep in population_analysis:
+for time_step in population_analysis:
     victories_percent_stack_x.append(i + 1)
 
     total_wins = 0
 
-    for agent_analysis in timestep:
-        wins, percent_to_win, percent_to_cooperate = agent_analysis    
+    for agent_analysis in time_step:
+        wins, percent_to_win, percent_to_cooperate = agent_analysis
 
         total_wins += percent_to_win
 
     j = 0
-    for agent_analysis in timestep:
+    for agent_analysis in time_step:
         wins, percent_to_win, percent_to_cooperate = agent_analysis
 
         victories_percent = 0
@@ -345,29 +351,32 @@ ax1.stackplot(victories_percent_stack_x, victories_percent_stack_y, colors=victo
 ax2 = fig.add_subplot(122)
 
 for i in range(len(victories_percent_stack_y)):
-    ax2.plot(victories_percent_stack_x, victories_percent_stack_y[i], c=victories_percent_stack_colors[i], linewidth=8, alpha=0.95)
+    ax2.plot(victories_percent_stack_x, victories_percent_stack_y[i], c=victories_percent_stack_colors[i], linewidth=1,
+             alpha=0.95)
 
 plt.show()
 
-# Testing mode with human
-while True:
-    state1 = [] # State visible to player 1 (actions of player 2)
-    state2 = [] # State visible to player 2 (actions of player 1)
+# Testing mode
+wins_p1 = 0
+wins_p2 = 0
+while (wins_p1 and wins_p2) <= 100:
+    state1 = []  # State visible to player 1 (actions of player 2)
+    state2 = []  # State visible to player 2 (actions of player 1)
 
     # Use a human to serve as player 1
-    player1 = Agent_Human()
+    player1 = AgentDefined(1)
 
     # Use a random AI to serve as player 2
     player2 = random.choice(population)
 
     for i in range(episode_length):
-        action = None
+        # #####actions = None
 
-        action1 = player1.pick_action(state1) # Allow player 1 to pick action
-        action2 = player2.pick_action(state2) # Select action for player 2
+        action1 = player1.pick_action(state1)  # Allow player 1 to pick action
+        action2 = player2.pick_action(state2)  # Select action for player 2
 
-        state1.append(action2) # Log action of player 2 for player 1
-        state2.append(action1) # Log action of player 1 for player 2
+        state1.append(action2)  # Log action of player 2 for player 1
+        state2.append(action1)  # Log action of player 1 for player 2
 
     total_reward1 = 0
     total_reward2 = 0
@@ -376,20 +385,20 @@ while True:
         action1 = state2[i]
         action2 = state1[i]
 
-        reward1 = 0 # Total reward due to the actions of player 1 in the entire episode
-        reward2 = 0 # Total reward due to the actions of player 2 in the entire episode
+        reward1 = 0  # Total reward due to the actions of player 1 in the entire episode
+        reward2 = 0  # Total reward due to the actions of player 2 in the entire episode
 
         # Calculate rewards for each player
-        if action1 == 0 and action2 == 0: # Both players cooperate
+        if action1 == 0 and action2 == 0:  # Both players cooperate
             reward1 = reward_matrix[0][0][0]
             reward2 = reward_matrix[0][0][1]
-        elif action1 == 0 and action2 == 1: # Only player 2 defects
+        elif action1 == 0 and action2 == 1:  # Only player 2 defects
             reward1 = reward_matrix[0][1][0]
             reward2 = reward_matrix[0][1][1]
-        elif action1 == 1 and action2 == 0: # Only player 1 defects
+        elif action1 == 1 and action2 == 0:  # Only player 1 defects
             reward1 = reward_matrix[0][2][0]
             reward2 = reward_matrix[0][2][1]
-        elif action1 == 1 and action2 == 1: # Both players defect
+        elif action1 == 1 and action2 == 1:  # Both players defect
             reward1 = reward_matrix[0][3][0]
             reward2 = reward_matrix[0][3][1]
 
@@ -399,8 +408,13 @@ while True:
     # Print the winning player and score
     print("Score: " + str(total_reward1) + " to " + str(total_reward2))
     if total_reward1 > total_reward2:
-        print("You win!")
+        print("Player 1 wins!")
+        wins_p1 += 1
     elif total_reward2 > total_reward1:
-        print("You lose!")
+        print("Player 2 loses!")
+        wins_p2 += 1
     else:
         print("Tie!")
+        wins_p1 += 0.5
+        wins_p2 += 0.5
+print("P1: {0}, P2: {1}".format(str(wins_p1), str(wins_p2)))
